e87b793eeebd6a3dd70e9d81bcadf147bb2f86b1
TongQuangHung102 <hungcma2002@gmail.com>
2025-01-03 19:37:46 +0700

    add alias, scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import Hello from '@components/Hello/Hello';
+import './App.css';
+import MainLayout from '@components/Layout/Layout';
+import MyHeader from '@components/Header/Header';
+import MyFooter from '@components/Footer/Footer';
+import MyButton from './components/Button/Button';
 
 function App() {
-  return <div>Ecommerse</div>;
+    return (
+        <>
+            <MainLayout>
+                {/* <MyHeader />
+                Content
+                <MyFooter /> */}
+                <MyButton />
+            </MainLayout>
+        </>
+    );
 }
 
 export default App;
